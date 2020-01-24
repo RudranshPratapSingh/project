0c6b58f1ec441f878d7b934f13c1cf51cb62ecd2
ashim-bhatta <60122258+ashim-bhatta@users.noreply.github.com>
2020-01-24 08:26:55 +0545

    Add files via upload

--- singer-web/js/main.js
+++ singer-web/js/main.js
@@ -1,3 +1,7 @@
+var header = document.getElementById("menu-list");
+var btns = header.getElementsByClassName("menu-list-item");
+var isNavOpen = false;
+
 var typeString = ['Hi', 'Hello', 'Hola', '你好','Salut'];
       var  i = 0;
       var count = 0
@@ -27,12 +31,51 @@ var typeString = ['Hi', 'Hello', 'Hola', '你好','Salut'];
       }
 
 
-
+  
 
 openMenu =()=>{
-  document.getElementById('mobile-menu').style.width='100%'
+  if(isNavOpen===false){
+    document.getElementById('mobile-menu').style.height='100%'
+    document.getElementById('mobile-menu').style.position='fixed'
+    isNavOpen = true;
+  }
+  else{
+    closeMenu()
+  }
+
+
 }
 
 closeMenu =()=>{
-  document.getElementById('mobile-menu').style.width='00%'
+  document.getElementById('mobile-menu').style.position='absolute'
+
+  document.getElementById('mobile-menu').style.height='00%'
+  isNavOpen = false;
+}
+
+
+// Change Active class\
+
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
+
+
+var navbar = document.querySelector('nav')
+
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
 }
