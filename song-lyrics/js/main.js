9f352a2e1bd109cc58361e7844c9f1f998cf1a35
ashim-bhatta <60122258+ashim-bhatta@users.noreply.github.com>
2020-02-16 08:42:02 +0545

    Add files via upload

---
+++ song-lyrics/js/main.js
@@ -0,0 +1,50 @@
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+
+
+/// api URL ///
+const apiURL = 'https://api.lyrics.ovh';
+
+
+/// adding event listener in form
+
+form.addEventListener('submit', e=> {
+    e.preventDefault();
+    searchValue = search.value.trim()
+
+    if(!searchValue){
+        alert("There is nothing to search")
+    }
+    else{ 
+        searchSong(searchValue)
+    }
+})
+
+
+//search song 
+async function searchSong(searchValue){
+    const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`)
+    const data = await searchResult.json();
+
+    // console.log(finaldata)
+    showData(data)
+}
+
+//display final result in DO
+function showData(data){
+  
+    result.innerHTML = `
+    <ul class="song-list">
+      ${data.data
+        .map(song=> `<li>
+                    <div>
+                        <strong>${song.artist.name}</strong> -${song.title} 
+                    </div>
+                    <span> get lyrics</span>
+                </li>`
+        )
+        .join('')}
+    </ul>
+  `;
+}
