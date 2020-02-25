83d6b3e0054e0287434def1b18101c133ca4c356
ashim-bhatta <60122258+ashim-bhatta@users.noreply.github.com>
2020-02-25 21:51:11 +0545

    Add files via upload

---
+++ main-portfolio/js/main.js
@@ -0,0 +1,21 @@
+// let x = 1
+
+
+// const rightArrow = () =>{
+//     x = x + 1
+//     if (x ===3){
+//         x = 1
+//     } 
+//     let element = 'sec-'+x
+//     document.getElementById(element).classList.replace('','width')
+//     alert(x)
+// }
+// const leftArrow = () =>{
+//     x = x - 1
+//     if(x===0){
+//         x=2
+//     }
+//     let element = 'sec-'+x
+//     document.getElementById(element).classList.toggle('width')
+//     alert(x)
+// }
