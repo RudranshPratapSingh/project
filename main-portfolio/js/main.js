01b2928bd61e328bf3f6e7ffb5b34280762796f0
ashim-bhatta <60122258+ashim-bhatta@users.noreply.github.com>
2020-02-26 17:40:57 +0545

    Add files via upload

--- main-portfolio/js/main.js
+++ main-portfolio/js/main.js
@@ -1,21 +1,5 @@
-// let x = 1
-
-
-// const rightArrow = () =>{
-//     x = x + 1
-//     if (x ===3){
-//         x = 1
-//     } 
-//     let element = 'sec-'+x
-//     document.getElementById(element).classList.replace('','width')
-//     alert(x)
-// }
-// const leftArrow = () =>{
-//     x = x - 1
-//     if(x===0){
-//         x=2
-//     }
-//     let element = 'sec-'+x
-//     document.getElementById(element).classList.toggle('width')
-//     alert(x)
-// }
+const checkAbout = () =>{
+    sessionStorage.setItem("pg", "02");
+    document.getElementById('active-pg').innerHTML =sessionStorage.getItem("pg");
+    
+}
