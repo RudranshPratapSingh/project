31cf0037ce8b02875aa8dc33950aa870bb8e9bbb
ashim-bhatta <60122258+ashim-bhatta@users.noreply.github.com>
2020-03-06 17:17:29 +0545

    Add files via upload

--- creative/js/main.js
+++ creative/js/main.js
@@ -16,3 +16,24 @@ const menuToogle = () =>{
         isMenuOpen = true
     }
 }
+
+
+// chage nav color on scroll
+
+var navbar = document.querySelector('nav')
+
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 200) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+
+//   if (window.pageYOffset > 300) {
+//     document.getElementById("myDIV").classList.add('none')
+// } else {
+//     document.getElementById("myDIV").classList.remove('none')
+//   }
+}
